3d971531f3b4905530619e65859ec87f1d646480
Timothée PC perso <timothee.viardtv@gmail.com>
2023-06-15 19:48:23 +0200

    Update servo + capteurs

--- f103-master/appli/servo.c
+++ f103-master/appli/servo.c
@@ -1,8 +1,11 @@
 /*
- * servo.c
- *
- *  Created on: 28 mars 2019
- *      Author: Nirgal
+ *******************************************************************************
+ * @file    servo.c
+ * @brief   Fonctions du servo-moteur.
+ * @author  Clémence MAIGNAN - Timothée VIARD
+ * @details Ce fichier contient les fonctions pour initialiser et contrôler un servo-moteur.
+ * @date    15 mars 2023
+ *******************************************************************************
  */
 #include "servo.h"
 #include "config.h"
@@ -12,82 +15,77 @@
 
 #define PERIOD_TIMER 10 //ms
 
-
-void SERVO_init2(Servo servo){
+/**
+  * @brief  Initialise un servo-moteur.
+  * @param  servo : Structure représentant le servo-moteur à initialiser.
+  * @retval None
+  */
+void SERVO_init(Servo servo){
 	//initialisation et lancement du timer1 � une periode de 10 ms
 	TIMER_run_us(servo.timer, PERIOD_TIMER*1000, FALSE); //10000us = 10ms
 	//activation du signal PWM sur le canal 1 du timer 1 (broche PA8)
 	TIMER_enable_PWM(servo.timer, servo.channel, 150, FALSE, FALSE);
 	//rapport cyclique regle pour une position servo de 50
-	SERVO_set_position(150, servo);
-
+	SERVO_set_position(100, servo);
 }
 
-
-
+/**
+  * @brief  Définit la position du servo-moteur.
+  * @param  position : Position du servo-moteur à définir.
+  * @param  servo : Structure représentant le servo-moteur.
+  * @retval None
+  */
 void SERVO_set_position(int16_t position, Servo servo){
-
-	if(position > 0){
-		if(position > 100){
-			position = 100; //ecretage si l'utilisateur demande plus de 100
-		}
-		servo.current_position =position;
-
-		TIMER_set_duty(servo.timer,servo.channel, position + 100); //définit la position du servo
-	}
-	else if(position < 0){
-		if(position < -100){
-			position = -100; //ecretage si l'utilisateur demande moins de -100
-		}
-		servo.current_position = position;
-
-		TIMER_set_duty(servo.timer,servo.channel, position + 200); //définit la position du servo
-	}
-	else{
-		TIMER_set_duty(servo.timer,servo.channel, position + 100);
-	}
+	servo.current_position = position;
+	TIMER_set_duty(servo.timer,servo.channel, servo.current_position);
 }
 
-
+/**
+  * @brief  Obtient la position actuelle du servo-moteur.
+  * @param  servo : Structure représentant le servo-moteur.
+  * @retval Position actuelle du servo-moteur.
+  */
 uint16_t SERVO_get_position(Servo servo){
 	return servo.current_position;
 }
 
+/**
+  * @brief  Calcule la différence entre la valeur maximale et minimale d'un capteur.
+  * @param  capt : Structure représentant le capteur.
+  * @retval Différence entre la valeur maximale et minimale du capteur.
+  */
 uint16_t capteurDelta(Capteur capt){
 	return capt.max - capt.min;
 }
 
-void SERVO_main(Capteur capt, Servo servo){
-
-	if(capteurDelta(capt) != 0){
-		capt.position = ((capt.value - capt.min) * 100) / capteurDelta(capt);
-
-		if (capt.position > 75){
-			SERVO_set_position(100, servo);
-		}
-
-		else if (capt.position < 25){
-			SERVO_set_position(-100, servo);
-		}
-
-		else{
-			SERVO_set_position(0, servo);
-		}
+/**
+  * @brief  Convertit la position du capteur en rapport cyclique du servo-moteur.
+  * @param  servo : Structure représentant le servo-moteur.
+  * @param  capt : Structure représentant le capteur.
+  * @retval Rapport cyclique correspondant à la position du capteur.
+  */
+uint16_t SERVO_position_to_duty(Servo servo, Capteur capt){
+	if(servo.sens){
+		return servo.min - (servo.min - servo.max) * (100 - capt.position) / 100;
 	}
-	HAL_Delay(100);
+	else{
+		return servo.min + (servo.max - servo.min) * (100 - capt.position) / 100;
+	}
+
 }
 
-void SERVO_main_inverse(int16_t position, Servo servo){
+/**
+  * @brief  Fonction principale de contrôle du servo-moteur en fonction de la position du capteur.
+  * @param  capt : Structure représentant le capteur.
+  * @param  servo : Structure représentant le servo-moteur.
+  * @retval None
+  */
+void SERVO_main(Capteur capt, Servo servo){
+	if(capteurDelta(capt) != 0){
+		capt.position = ((capt.value - capt.min) * 100) / capteurDelta(capt);
+		uint16_t duty = SERVO_position_to_duty(servo, capt);
 
-	if (position > 900){
-		SERVO_set_position(-100, servo);
-	}
-	else if (position < 900 && position>550){
-		SERVO_set_position(50, servo);
-	}
-
-	else if (position<550){
-		SERVO_set_position(100, servo);
+		SERVO_set_position(duty, servo);
 	}
 	HAL_Delay(100);
 }
--- f103-master/appli/servo.h
+++ f103-master/appli/servo.h
@@ -10,29 +10,82 @@
 #define SERVO_H_
 
 #include "macro_types.h"
-#include "stm32f1xx.h"
+#include "stm32f103xb.h"
 #include "stm32f1_timer.h"
+#include "stdbool.h"
 
+
+/**
+ * \struct Servo
+ * Structure représentant un servo-moteur.
+ */
 typedef struct {
-    uint16_t current_position;
-    uint32_t channel;
-    timer_id_e timer;
+    uint16_t current_position; /**< Position actuelle du servo-moteur */
+    uint32_t channel; /**< Canal du timer utilisé pour le servo-moteur */
+    timer_id_e timer; /**< Identifiant du timer utilisé pour le servo-moteur */
+    bool sens; /**< Sens de rotation du servo-moteur */
+    uint16_t max; /**< Position maximale du servo-moteur */
+    uint16_t min; /**< Position minimale du servo-moteur */
 } Servo;
 
+/**
+ * \struct Capteur
+ * Structure représentant un capteur.
+ */
 typedef struct {
-    uint32_t value;
-    uint16_t max;
-    uint16_t min;
-    uint16_t position;
+    uint32_t value; /**< Valeur du capteur */
+    uint16_t max; /**< Valeur maximale du capteur */
+    uint16_t min; /**< Valeur minimale du capteur */
+    uint16_t position; /**< Position du capteur */
 } Capteur;
 
+/**
+ * \brief Initialise un servo-moteur.
+ * \param servo Le servo-moteur à initialiser.
+ */
+void SERVO_init(Servo servo);
 
-void SERVO_init(void);
-void SERVO_init2(Servo servo);
+/**
+ * \brief Définit la position d'un servo-moteur.
+ * \param position La position à définir.
+ * \param servo Le servo-moteur concerné.
+ */
 void SERVO_set_position(int16_t position, Servo servo);
+
+/**
+ * \brief Fonction de test du module SERVO.
+ */
 void SERVO_process_test(void);
+
+/**
+ * \brief Obtient la position actuelle d'un servo-moteur.
+ * \param servo Le servo-moteur concerné.
+ * \return La position actuelle du servo-moteur.
+ */
 uint16_t SERVO_get_position(Servo servo);
+
+/**
+ * \brief Calcule la différence entre deux positions d'un capteur.
+ * \param capt Le capteur contenant les positions.
+ * \return La différence entre les positions du capteur.
+ */
 uint16_t capteurDelta(Capteur capt);
+
+/**
+ * \brief Fonction de test.
+ */
 void test(void);
 
-#endif /* SERVO_H_ */
+/**
+ * \brief Gère le servo-moteur en fonction d'un devoir (duty) donné.
+ * \param duty Le devoir (duty) à appliquer.
+ * \param servo Le servo-moteur à gérer.
+ */
+void SERVO_manager(float duty, ServoSpec * servo);
+
+/**
+ * \brief Convertit une position de servo-moteur en devoir (duty) correspondant en fonction d'un capteur.
+ * \param servo Le servo-moteur concerné.
+ * \param capt Le capteur utilisé pour la conversion.
+ * \return Le devoir (duty) correspondant à la position du servo-moteur.
+ */
